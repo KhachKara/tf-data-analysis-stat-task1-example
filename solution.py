c3572bfa9ac2e9ca25821fc41e6331a962d3f2e0
Khachatur <khachkara@gmail.com>
2023-04-04 21:55:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,7 +4,6 @@ import numpy as np
 chat_id = 42791670 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    n = len(x)
     ln_checks = np.log(x - 855)
-    alpha_hat = np.exp(np.mean(ln_checks))
+    alpha_hat = np.mean(ln_checks)
     return alpha_hat
